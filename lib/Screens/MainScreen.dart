d1b1c4f99f77bbe9a6a67716f7f4905d0cbf6ce6
Akhmad Aldo Sari <aldodevv@MacBook-Pro-Akhmad.local>
2022-10-25 13:45:46 +0700

    upload vigenesia

---
+++ lib/Screens/MainScreen.dart
@@ -0,0 +1,94 @@
+import 'dart:convert';
+import 'package:vigenesia/Screens/EditPage.dart';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'Login.dart';
+import 'package:vigenesia/Constant/const.dart';
+import 'package:another_flushbar/flushbar.dart';
+
+class MainScreens extends StatefulWidget {
+  final String? nama;
+  const MainScreens({Key? key, this.nama}) : super(key: key);
+  @override
+  _MainScreensState createState() => _MainScreensState();
+}
+
+class _MainScreensState extends State<MainScreens> {
+  TextEditingController isiController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+// <-- Berfungsi Untuk Bisa Scroll
+        child: SafeArea(
+// < -- Biar Gak Keluar Area Screen HP
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment
+                      .center, // <-- Berfungsi untuk atur nilai X jadi tengah
+                  children: [
+                    SizedBox(
+                      height: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          "Hallo ${widget.nama}",
+                          style: TextStyle(
+                              fontSize: 22, fontWeight: FontWeight.w500),
+                        ),
+                        TextButton(
+                            child: Icon(Icons.logout),
+                            onPressed: () {
+                              Navigator.pop(context);
+                              Navigator.push(
+                                  context,
+                                  new MaterialPageRoute(
+                                      builder: (BuildContext context) =>
+                                          new Login()));
+                            })
+                      ],
+                    ),
+                    SizedBox(height: 20), // <-- Kasih Jarak Tinggi : 50px
+                    FormBuilderTextField(
+                      controller: isiController,
+                      name: "isi_motivasi",
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        contentPadding: EdgeInsets.only(left: 10),
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                          onPressed: () async {
+                            print("Sukses");
+                          },
+                          child: Text("Submit")),
+                    ),
+                    SizedBox(
+                      height: 40,
+                    ),
+                    TextButton(
+                      child: Icon(Icons.refresh),
+                      onPressed: () {
+// _getData();
+                      },
+                    ),
+                  ]),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/Register.dart
@@ -0,0 +1,94 @@
+import 'package:vigenesia/Screens/Login.dart'; //kebutuhan form builder
+import 'package:flutter/material.dart';
+import 'package:another_flushbar/flushbar.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:dio/dio.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+  @override
+  _RegisterState createState() => _RegisterState();
+}
+
+class _RegisterState extends State<Register> {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController profesiController = TextEditingController();
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Center(
+            child: Container(
+              width: MediaQuery.of(context).size.width / 1.3,
+              height: MediaQuery.of(context).size.height,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Register Your Account",
+                    style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+                  ),
+                  SizedBox(height: 50),
+                  FormBuilderTextField(
+                    name: "name",
+                    controller: nameController,
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 10),
+                        border: OutlineInputBorder(),
+                        labelText: "Nama"),
+                  ),
+                  SizedBox(height: 20),
+                  FormBuilderTextField(
+                    name: "profesi",
+                    controller: profesiController,
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 10),
+                        border: OutlineInputBorder(),
+                        labelText: "Profesi"),
+                  ),
+                  SizedBox(height: 20),
+                  FormBuilderTextField(
+                    name: "email",
+                    controller: emailController,
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 10),
+                        border: OutlineInputBorder(),
+                        labelText: "Email"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  FormBuilderTextField(
+                    obscureText:
+                        true, // <-- Buat bikin setiap inputan jadi bintang " * "
+                    name: "password",
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        contentPadding: EdgeInsets.only(left: 10),
+                        border: OutlineInputBorder(),
+                        labelText: "Password"),
+                  ),
+                  SizedBox(
+                    height: 30,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: ElevatedButton(
+                        onPressed: () async {
+                          print("blok");
+                        },
+                        child: Text("Daftar")),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
